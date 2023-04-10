e21ba8a205c38bb74ac7e9be1f6250aaee40410c
Jordan Kanchanda <arrhythmixmusic@gmail.com>
2023-04-10 10:13:28 -0700

    Pre-Release Final #2

--- Contents/mods/AmmoNamingConventionPlus3/media/lua/client/CVAS_7Swatpack.lua
+++
@@ -1,7 +0,0 @@
-require("CVAS_1Tweakers");
-
-if getActivatedMods():contains("Swatpack") then
-	TweakItemCVAS("Base.RubberShells","DisplayCategory","AmmoShell");
-	TweakItemCVAS("Base.RubberShellsBox","DisplayCategory","AmmoBox");
-	TweakItemCVAS("Base.9mmMp5Clip","DisplayCategory","AmmoBox");
-end
