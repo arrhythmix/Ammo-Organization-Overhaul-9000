b9240114c44254147076ecce04f855e86b82d1b2
Jordan Kanchanda <arrhythmixmusic@gmail.com>
2023-04-09 19:25:08 -0700

    AmmoNCPlus #1

---
+++ Contents/mods/AmmoNamingConventionPlus/media/lua/client/CVAS_7Swatpack.lua
@@ -0,0 +1,7 @@
+require("CVAS_1Tweakers");
+
+if getActivatedMods():contains("Swatpack") then
+	TweakItemCVAS("Base.RubberShells","DisplayCategory","AmmoShell");
+	TweakItemCVAS("Base.RubberShellsBox","DisplayCategory","AmmoBox");
+	TweakItemCVAS("Base.9mmMp5Clip","DisplayCategory","AmmoBox");
+end
